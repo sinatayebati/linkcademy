7b66acfefc2908951bb878b8d6776332b9ebb509
sinatayebati <sinatayebati@gmail.com>
2023-11-16 01:34:39 -0600

    commit #5 header committed

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
-
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 
 function Header() {
   return (
     <div className='header'>
 
         <div className='header__left'>
-          <img src={require('./assets/LinkAcademy-logos.jpg')} alt="" />
+          <img src={require('./assets/logo-02/LinkAcademy-logos_transparent.png')} alt="" />
 
           <div className='header__search'>
             <SearchIcon />
@@ -17,7 +19,8 @@ function Header() {
         </div>
 
         <div className='header__right'>
-
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
         </div>
     </div>
   )
---
+++ src/HeaderOption.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./HeaderOption.css";
+
+function HeaderOption({ Icon, title }) {
+  return (
+    <div className='headerOption'>
+      {Icon && <Icon className='headerOption__icon' />}
+      <h3 className='headerOption__title'>{title}</h3>
+    </div>
+  )
+}
+
+export default HeaderOption
