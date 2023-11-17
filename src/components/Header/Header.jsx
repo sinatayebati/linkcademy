79f1ec66ceb4fca4f3258a38d7dfa1402aa4af2e
sinatayebati <sinatayebati@gmail.com>
2023-11-17 00:30:22 -0600

    commit #8 style updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header() {
   return (
@@ -11,7 +14,7 @@ function Header() {
 
         <div className='header__left'>
           {/* <img src={require('./assets/logo-02/LinkAcademy-logos_transparent.png')} alt="" /> */}
-          <div className="home__img"></div>
+          <div className='home__img'></div>
 
           <div className='header__search'>
             <SearchIcon />
@@ -22,6 +25,10 @@ function Header() {
         <div className='header__right'>
           <HeaderOption Icon={HomeIcon} title="Home" />
           <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+          <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+          <HeaderOption Icon={ChatIcon} title="Chat"/>
+          <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+          <HeaderOption avatar="https://github.com/sinatayebati/sinatayebati.github.io/tree/master/src/assets/about2.png" title="Me"/>
         </div>
     </div>
   )
--- src/components/Header/HeaderOption.jsx
+++ src/components/Header/HeaderOption.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./HeaderOption.css";
+import { Avatar } from '@mui/material';
 
-function HeaderOption({ Icon, title }) {
+function HeaderOption({ avatar, Icon, title }) {
   return (
     <div className='headerOption'>
       {Icon && <Icon className='headerOption__icon' />}
+      {avatar && (
+        <Avatar className='headerOption__icon' src={avatar}/>
+      )}
       <h3 className='headerOption__title'>{title}</h3>
     </div>
   )
