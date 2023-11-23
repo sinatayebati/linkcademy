7a6e929c1052c2bc6ac3b8037bfdc20adac9cd5e
sinatayebati <sinatayebati@gmail.com>
2023-11-22 22:52:44 -0600

    commit #17 input options committed

--- src/components/Sidebar/Feed.jsx
+++ src/components/Sidebar/Feed.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Feed.css';
 import CreateIcon from '@mui/icons-material/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
 function Feed() {
   return (
@@ -12,6 +17,12 @@ function Feed() {
                 <input type="text" />
                 <button type='submit'>Send</button>
             </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9"/>
+          <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E"/>
+          <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD"/>
+          <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E"/>
         </div>
       </div>
     </div>
