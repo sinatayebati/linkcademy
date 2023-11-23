e4f83d2686cbb462de43fe86e55a3eae4268a7ec
sinatayebati <sinatayebati@gmail.com>
2023-11-22 23:43:39 -0600

    commit #19 post section added

--- src/components/Sidebar/Post.jsx
+++ src/components/Sidebar/Post.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Post.css';
 import { Avatar } from '@mui/material';
+import InputOption from './InputOption';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 
 function Post({ name, description, message, photoUrl}) {
   return (
@@ -8,13 +10,18 @@ function Post({ name, description, message, photoUrl}) {
       <div className="post__header">
         <Avatar />
         <div className="post__info">
-            <h2>Sina Tayebati</h2>
-            <p>Description</p>
+            <h2>{name}</h2>
+            <p>{description}</p>
         </div>
       </div>
 
       <div className="post__body">
-        <p>Message goes here</p>
+        <p>{message}</p>
+      </div>
+
+      <div className="post__buttons">
+        <InputOption Icon={ThumbUpOffAltIcon} title='like' color='gray'/>
+        <InputOption Icon={ThumbUpOffAltIcon} title='like' color='gray'/>
       </div>
     </div>
   )
