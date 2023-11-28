518c8f83ec464a735ebe9aeac058daf693804939
sinatayebati <sinatayebati@gmail.com>
2023-11-27 23:17:57 -0600

    commit #21 firebase integration

--- src/components/Sidebar/Feed.jsx
+++ src/components/Sidebar/Feed.jsx
@@ -11,6 +11,10 @@ import Post from './Post';
 function Feed() {
   const [posts, setPosts] = useState([]);
 
+  const sendPost = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className='feed'>
       <div className="feed__inputContainer">
@@ -18,7 +22,7 @@ function Feed() {
             <CreateIcon />
             <form>
                 <input type="text" />
-                <button type='submit'>Send</button>
+                <button onClick={sendPost} type='submit'>Send</button>
             </form>
         </div>
         <div className="feed__inputOptions">
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1nlAPRBQRaiPi7vP_JLWu9d2w3Du_pYw",
+    authDomain: "linkcademy.firebaseapp.com",
+    projectId: "linkcademy",
+    storageBucket: "linkcademy.appspot.com",
+    messagingSenderId: "539411994095",
+    appId: "1:539411994095:web:6d1e78622baab74c83d21a",
+    measurementId: "G-7850E56NED"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
