03b234c9989b12cdd46db41d6c20bdedc807b2da
sinatayebati <sinatayebati@gmail.com>
2023-12-11 22:47:49 -0600

    commit #33 login commits

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
 
           <div className='header__search'>
             <SearchIcon />
-            <input type="text" />
+            <input placeholder='Search' type="text" />
           </div>
         </div>
 
