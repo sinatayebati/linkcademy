3669747a4664a4e35149f0a5454b51a57d8a53a5
sinatayebati <sinatayebati@gmail.com>
2023-12-11 22:41:29 -0600

    commit #32 login commits

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,12 +3,23 @@ import './Login.css';
 import Logo from "../../assets/logo-02/LinkAcademy-logos_transparent.png"
 
 function Login() {
+    const loginToApp = () => {};
+    const register = () => {};
   return (
     <div className='login'>
         <img src={Logo} alt=""/>
         <form>
             <input placeholder='Full name (required if registering)' type="text" />
+            <input placeholder='Profile pic url (optional)' type="text" />
+            <input placeholder='Email' type="email" />
+            <input placeholder='Password' type="password" />
+            <button type='submit' onClick={loginToApp}>Sign In</button>
         </form>
+
+        <p>
+            Not a member?
+            <span className='login__register' onClick={register}>Register Now</span>
+        </p>
     </div>
   )
 }
