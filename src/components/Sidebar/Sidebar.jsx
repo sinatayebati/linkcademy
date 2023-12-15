73491e46dd4eb6b758356f392ec1338ca236c784
sinatayebati <sinatayebati@gmail.com>
2023-12-14 22:38:13 -0600

    commit #41 user info update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import './Sidebar.css';
 import { Avatar } from '@mui/material';
 import AboutImg from "../../assets/avatar/background.jpeg";
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice';
 
 function Sidebar() {
+    const user = useSelector(selectUser)
 
     const recentItem = (topic) => (
         <div className="sidebar__recentItem">
@@ -17,9 +20,9 @@ function Sidebar() {
 
         <div className="sidebar__top">
             <img src={AboutImg} alt="" />
-            <Avatar className='sidebar__avatar'/>
-            <h2>Sina Tayebati</h2>
-            <h4>stayeb3@uic.edu</h4>
+            <Avatar src={user.photoUrl} className='sidebar__avatar'>{user.email[0]}</Avatar>
+            <h2>{user.displayName}</h2>
+            <h4>{user.email}</h4>
         </div>
 
         <div className="sidebar__stats">
