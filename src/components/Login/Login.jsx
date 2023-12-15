281956105711a81b730fa7a0f34dc2ecc1a0759e
sinatayebati <sinatayebati@gmail.com>
2023-12-14 19:40:56 -0600

    commit #35 login value tracing

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import Logo from "../../assets/logo-02/LinkAcademy-logos_transparent.png"
+import { auth } from '../../firebase';
 
 function Login() {
-    const loginToApp = () => {};
-    const register = () => {};
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+
+  const register = () => {
+  };
+
   return (
     <div className='login'>
         <img src={Logo} alt=""/>
         <form>
-            <input placeholder='Full name (required if registering)' type="text" />
-            <input placeholder='Profile pic url (optional)' type="text" />
-            <input placeholder='Email' type="email" />
-            <input placeholder='Password' type="password" />
+            <input 
+             value={name}
+             onChange={e => setName(e.target.value)}
+             placeholder='Full name (required if registering)' 
+             type="text" 
+            />
+
+            <input 
+             value={profilePic} 
+             onChange={e => setProfilePic(e.target.value)}
+             placeholder='Profile pic url (optional)' 
+             type="text" 
+            />
+
+            <input
+             value={email}
+             onChange={e => setEmail(e.target.value)}
+             placeholder='Email'
+             type="email"
+            />
+
+            <input 
+             value={password}
+             onChange={e => setPassword(e.target.value)}
+             placeholder='Password' 
+             type="password" 
+            />
+
             <button type='submit' onClick={loginToApp}>Sign In</button>
         </form>
 
