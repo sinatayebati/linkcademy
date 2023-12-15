f5fea5aba33e5f9f9f03e44fc1c296262ce07322
sinatayebati <sinatayebati@gmail.com>
2023-12-14 22:15:35 -0600

    commit #39 login function added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -14,6 +14,17 @@ function Login() {
 
   const loginToApp = (e) => {
     e.preventDefault();
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then(userAuth => {
+      dispatch(login({
+        email: userAuth.user.email,
+        uid: userAuth.user.uid,
+        displayName: userAuth.user.displayName,
+        profileUrl: userAuth.user.photoURL,
+      }))
+    })
+
   };
 
   const register = () => {
