578bb84d1a4639a9d20ec25f504bbb2767413f00
sinatayebati <sinatayebati@gmail.com>
2023-12-15 18:35:28 -0600

    commit #47 widget bar update

--- src/components/Widgets/Widget.jsx
+++ src/components/Widgets/Widget.jsx
@@ -3,6 +3,23 @@ import "./Widget.css";
 import InfoIcon from '@mui/icons-material/Info';
 
 function Widget() {
+
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+
+      <div className="widgets__articleleft">
+        
+      </div>
+
+      <div className="widgets__articleright">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+
+    </div>
+  )
+
+
   return (
     <div className='widgets'>
       <div className="widgets__header">
